d4f60109b02a0f5a5b0779d60020d27e537cd895
Andrés López Albín <andres.albin93@gmail.com>
2019-12-13 13:53:38 +0100

    changed welcome message

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var BASE_API_PATH = '/api/v1/';
 
 var app = express();
 
-app.get('/', (req, res) => res.send('<html><body><h1>My server</h1></body></html>'));
+app.get('/', (req, res) => res.send('<html><body><h1>Welcome to Tournaments microservice!</h1></body></html>'));
 
 
 
