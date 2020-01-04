9cdfeee887119ad04deb53ab3fbba4923f2147fd
joseenrique <joseenriqueruiznavarro@gmail.com>
2020-01-04 16:23:27 +0100

    added to swagger the JWT

--- docs/swagger/tournamentsSwagger.js
+++ docs/swagger/tournamentsSwagger.js
@@ -1,10 +1,17 @@
-    /**
+/**
+ * @typedef Tournament
+ * @property {string} name.required - match's location
+ * @property {string} type.required - tournament id
+ * @property {string} endDate.required - visitior team id
+ * @property {string} startDate.required - local team id
+ * @property {array} clasification.required - match date
+ */
+/**
  * This function comment is parsed by doctrine
  * @route GET /tournaments
  * @group Tournaments - Operations about user
- * @param {string} email.query.required - username or email - eg: user@domain
- * @param {string} password.query.required - user's password.
- * @param {string} password.query.required - user's balbala.
+ * @headers {string} 200.X-Expires-After - 	date in UTC when token expires
  * @returns {object} 200 - An array of user info
+ * @security JWT
  * @returns {Error}  default - Unexpected error
  */
